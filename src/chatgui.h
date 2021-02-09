c80a631b521c03d61d8313d92a5de79ccacd3917
Yuanzhe Dong <yuanzhedong@gmail.com>
2021-02-09 15:56:21 -0800

    task 1

--- src/chatgui.h
+++ src/chatgui.h
@@ -16,7 +16,8 @@ private:
     //// STUDENT CODE
     ////
 
-    ChatLogic *_chatLogic;
+    std::unique_ptr<ChatLogic>  _chatLogic;
+    //ChatLogic *_chatLogic;
 
     ////
     //// EOF STUDENT CODE
@@ -27,7 +28,7 @@ public:
     ~ChatBotPanelDialog();
 
     // getter / setter
-    ChatLogic *GetChatLogicHandle() { return _chatLogic; }
+    ChatLogic* GetChatLogicHandle() { return _chatLogic.get(); }
 
     // events
     void paintEvent(wxPaintEvent &evt);
